4e1c08197fbc25005436c6373e38048ac9a71093
Taufiiqulhakim <taufiiqulhakimr@gmail.com>
2026-01-20 21:56:15 +0700

    revise 1

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type Produk struct {
 var produk = []Produk{
 	{ID: 1, Nama: "Indomie Godog", Harga: 3500, Stok: 10},
 	{ID: 2, Nama: "Vit 1000ml", Harga: 3000, Stok: 40},
-	{ID: 3, Nama: "Kecap", Harga: 20000, Stok: 20},
+	{ID: 3, Nama: "Kecap", Harga: 12000, Stok: 20},
 }
 
 func getProdukByID(w http.ResponseWriter, r *http.Request) {
@@ -70,6 +70,7 @@ func updateProduk(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+
 	http.Error(w, "Produk belum ada", http.StatusNotFound)
 }
 
@@ -92,6 +93,7 @@ func deleteProduk(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+
 	http.Error(w, "Produk belum ada", http.StatusNotFound)
 }
 
